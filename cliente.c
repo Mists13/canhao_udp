1d26b0530a49a4729d0b8babfe301d6754e36b4d
Mists13 <lmists13@gmail.com>
2023-02-21 21:32:58 -0300

    Fixed some bugs, handled messages out of order, and missing messages

--- cliente.c
+++ cliente.c
@@ -7,65 +7,69 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(int argc, char *argv[]) 
+int main(int argc, char *argv[]) {
 
- {  int sockdescr;
-    int numbytesrecv;
-    struct sockaddr_in sa;
-    struct hostent *hp;
-    char buf[BUFSIZ+1];
-    char *host;
-    char *dados;
+  int sockdescr;
+  int numbytesrecv;
+  struct sockaddr_in sa;
+  struct hostent *hp;
+  char buf[BUFSIZ+1];
+  char *host;
+  char *dados;
 
-    unsigned int i;
+  unsigned int i;
 
-    if(argc != 4) {
-      puts("Uso correto: <cliente> <nome-servidor> <porta> <dados>");
-      exit(1);
+  if(argc != 4) {
+    puts("[Cliente] Uso correto: <cliente> <nome-servidor> <porta> <dados>");
+    exit(1);
+  }
+
+  host = argv[1];
+  int numMaximo = atoi(argv[3]);
+
+  if((hp = gethostbyname(host)) == NULL){
+    puts("[Cliente] Nao consegui obter endereco IP do servidor.");
+    exit(1);
+  }
+
+  bcopy((char *)hp->h_addr_list[0], (char *)&sa.sin_addr, hp->h_length);
+
+  sa.sin_family = hp->h_addrtype;
+  sa.sin_port = htons(atoi(argv[2]));
+
+  if((sockdescr=socket(hp->h_addrtype, SOCK_DGRAM, 0)) < 0) {
+    puts("[Cliente] Nao consegui abrir o socket.");
+    exit(1);
+  }
+
+  // Enviando numero total de mensagens
+  printf("[Cliente] Enviando o total %d de mensagens\n", numMaximo);
+  sprintf(buf, "%i", numMaximo);
+  if(sendto(sockdescr, buf, strlen(buf), 0, (struct sockaddr *) &sa, sizeof sa) != strlen(buf)){
+              puts("[Cliente] Nao consegui mandar os dados");
+              exit(1);
+  }
+  for(int i=1;i<=numMaximo;i++){
+    char buf[10];
+    sprintf(buf, "%i", i);
+      if(sendto(sockdescr, buf, strlen(buf), 0, (struct sockaddr *) &sa, sizeof sa) != strlen(buf)){
+        puts("[Cliente] Nao consegui mandar os dados"); 
+        exit(1);
     }
+    printf("[Cliente] Enviando: %d\n", atoi(buf));
+  }
+  puts("[Cliente] Enviando '0' para finalizar a sequencia\n");
+  sprintf(buf, "%i", 0);
+  if(sendto(sockdescr, buf, strlen(buf), 0, (struct sockaddr *) &sa, sizeof sa) != strlen(buf)){
+    puts("[Cliente] Nao consegui mandar os dados");
+    exit(1);
+  }
+  
+  /*for(int i=0;i<30;i++){
+    recvfrom(sockdescr, buf, BUFSIZ, 0, (struct sockaddr *) &sa, &i);
 
-    host = argv[1];
-    int numMaximo = atoi(argv[3]);
-
-    if((hp = gethostbyname(host)) == NULL){
-      puts("Nao consegui obter endereco IP do servidor.");
-      exit(1);
-    }
-
-    bcopy((char *)hp->h_addr_list[0], (char *)&sa.sin_addr, hp->h_length);
-
-    sa.sin_family = hp->h_addrtype;
-
-    sa.sin_port = htons(atoi(argv[2]));
-
-    if((sockdescr=socket(hp->h_addrtype, SOCK_DGRAM, 0)) < 0) {
-      puts("Nao consegui abrir o socket.");
-      exit(1);
-    }
-    for(int i=1;i<=numMaximo;i++){
-      char buf[10];
-      sprintf(buf, "%i", i);
-      	if(sendto(sockdescr, buf, strlen(buf), 0, (struct sockaddr *) &sa, sizeof sa) != strlen(buf)){
-      		puts("Nao consegui mandar os dados"); 
-      		exit(1);
-    	}
-	printf("enviando: %d\n", atoi(buf));
-    }
-    sprintf(buf, "%i", 0);
-    if(sendto(sockdescr, buf, strlen(buf), 0, (struct sockaddr *) &sa, sizeof sa) != strlen(buf)){
-                puts("Nao consegui mandar os dados");
-                exit(1);
-    }
-    sprintf(buf, "%i", numMaximo);
-    if(sendto(sockdescr, buf, strlen(buf), 0, (struct sockaddr *) &sa, sizeof sa) != strlen(buf)){
-                puts("Nao consegui mandar os dados");
-                exit(1);
-    }
-    /*for(int i=0;i<30;i++){
-    	recvfrom(sockdescr, buf, BUFSIZ, 0, (struct sockaddr *) &sa, &i);
-
-    	printf("Sou o cliente, recebi: %s\n", buf);
-    }*/
-    close(sockdescr);
-    exit(0);
+    printf("Sou o cliente, recebi: %s\n", buf);
+  }*/
+  close(sockdescr);
+  exit(0);
 }
--- servidor.c
+++ servidor.c
@@ -12,59 +12,90 @@
 #define MAXHOSTNAME 30
 
 
-void receiveMessages(char buf[], int socket_s, struct sockaddr_in cli_addr, int cli_addr_len,
-					 int messagesOutOfOrder[], int messagesNotReceivedYet[], int *intexErrors){
+void calculateMetrics(int numOutOfOrder, int numNotReceived, int lastMsgReceived, int totalMsgs){
 
-	int i = 0;
+	puts("\n");
+	printf("[Servidor] Calculando erros...\n");
+	printf("[Servidor] Total mensagens esperadas: %d\n", totalMsgs);
+
+	int totalNotReceived = totalMsgs - lastMsgReceived;
+	if (numNotReceived != 0 || totalNotReceived != 0){
+		printf("[Servidor] Total de mensagens não recebidas: %d\n", numNotReceived);
+		printf("[Servidor] Total de mensagens não recebidas após falha no servidor: %d\n", totalNotReceived);
+		printf("[Servidor] Porcentagem de mensagens não recebidas: %.2f%%\n", 
+				100*((double)(totalNotReceived+numNotReceived)/(double)totalMsgs));
+	}
+	if(numOutOfOrder != 0){
+		printf("[Servidor] Metricas de mensagens fora de ordem: \n");
+		printf("[Servidor] Total de mensagens fora de ordem: %d\n", numOutOfOrder);
+		printf("[Servidor] Ultima mensagem recebida: %d\n", lastMsgReceived);
+		printf("[Servidor] Porcentagem de mensagens fora de ordem: %.2f%%\n",
+		 		100*((double)numOutOfOrder/(double)totalMsgs));
+		
+	} 
+	if ( totalNotReceived == 0 && numOutOfOrder == 0 && numNotReceived == 0){
+		printf("[Servidor] Todas as mensagens foram recebidas corretamente \n");
+	}
+}
+
+
+void handleMessageOutOfOrder(int *numMessagesOutOfOrder, int *numMessagesNotReceivedYet,
+							 int *sequenceNum, int msgReceived, int lastMsgReceived){
+	
+	if (msgReceived < *sequenceNum){												// Msg chegou atrasadinha												
+		printf("[Servidor] Recebi a mensagem %d atrasadinha\n", msgReceived);
+		*numMessagesNotReceivedYet--; 											// remove das mensagens nao recebidas ainda
+		*numMessagesOutOfOrder++; 	 											// adiciona nas mensagens fora de ordem
+																				// num sequencia se mantem o mesmo
+
+	} else if (msgReceived - *sequenceNum > 1 )	{								// Seq se msgs perdidas
+		printf("[Servidor] Recebi a mensagem----> %d\n", msgReceived);	
+		printf("[Servidor] Esperava %d mensagens a partir da mensagem %d\n",
+				msgReceived - *sequenceNum, *sequenceNum);
+		*numMessagesNotReceivedYet+= (msgReceived - *sequenceNum);				// Anota seq de mensagens perdidas
+		*sequenceNum = msgReceived + 1; 							// Num seq se torna o seguinte ao ja recebido
+	}
+	else {
+		printf("[Servidor] Esperava a mensagem %d\n", 			*sequenceNum);
+		printf("[Servidor] Recebi a mensagem %d\n",   			msgReceived);
+		*numMessagesNotReceivedYet++;											// Msg ainda pode ser recebida			
+		*sequenceNum=+2;															// num de sequencia aumenta em 2
+	}
+
+}
+
+void receiveMessages(char buf[], int socket_s, struct sockaddr_in cli_addr, int cli_addr_len,
+					 int messagesOutOfOrder[], int messagesNotReceivedYet[], int totalMsgs){
+	int indexNotReceived = 0;
+	int indexOutOfOrder = 0;
+	int lastMsgReceived = 0;
+	int numNotReceived = 0;
+	int numOutOfOrder = 0;
 	int sequenceNum = 2;
-	int index = 0;
 
 	// Recebe sequencia ate encontrar 0
 	while(atoi(buf)!=0){
 		memset(buf,0, BUFSIZ);
 		if (recvfrom(socket_s, buf, BUFSIZ, 0, (struct sockaddr *) &cli_addr, &cli_addr_len) > 0){
 			if(atoi(buf)!=0){
-				printf("Sou o servidor, recebi a mensagem----> %d\n", atoi(buf));
-				if(atoi(buf)!=sequenceNum){
-					// saveMessagesNotSent(messagesNotSentYet, i, buf); // armazenar array
-					messagesNotReceivedYet[index] = sequenceNum;		// msg deveria ser enviada
-					messagesOutOfOrder[index] = atoi(buf);				// mensagem enviada na ordem errada
-					index++;
-					sequenceNum=+2;
+				if(atoi(buf)!=sequenceNum){				// Mensagem fora de ordem			
+					handleMessageOutOfOrder( &numOutOfOrder, &numNotReceived,
+							  				 &sequenceNum, atoi(buf), lastMsgReceived);
 				} else {
+					printf("[Servidor] Recebi a mensagem----> %d OK\n", atoi(buf));
 					sequenceNum++;
 				}
-			}	
+				lastMsgReceived = atoi(buf);
+				// verificar sequencia de mensagens perdidas
+			}
 		}else{
-			messagesOutOfOrder[index] = sequenceNum;
+			puts("[Servidor] Erro ao receber mensagens. Finalizando..");
 			memset(buf,'\0', BUFSIZ);
 		}
 	}
-	printf("fim de recebimento...\n");
-	memset(buf,'\0', BUFSIZ);
-}
-
-void calculateMetrics(char buf[], int socket_s, struct sockaddr_in cli_addr, int cli_addr_len,
-					 int messagesOutOfOrder[], int indexErrors){
-
-	// Receber numero de total de mensagens
-	recvfrom(socket_s, buf, BUFSIZ, 0, (struct sockaddr *) &cli_addr, &cli_addr_len);
-	int total=atoi(buf);
-	printf("calculando erros...\n");
-	printf("total mensagens esperadas: %d\n", total);
-
-	if(indexErrors != 0){
-		printf("mensagens erradas: \n");
-		for(int i=0; i<indexErrors; i++)
-			printf("%d ", messagesOutOfOrder[i]);
-		printf("\n");
-		printf("total de mensagens erradas: %d\n", indexErrors);
-		printf("porcentagem de mensagens erradas: %.2f%%\n", 100*((double)indexErrors/(double)total));
-	}
-	else{
-		printf("nao tem mensagens erradas\n");
-	}
-	memset(messagesOutOfOrder,'\0', BUFSIZ);
+	
+	printf("[Servidor] Fim de recebimento...\n");
+	calculateMetrics(numOutOfOrder, numNotReceived, lastMsgReceived, totalMsgs);
 	memset(buf,'\0', BUFSIZ);
 }
 
@@ -77,7 +108,8 @@ int main ( int argc, char *argv[] ){
 	struct hostent *hp;
 	char localhost [MAXHOSTNAME];
 	int indexErrors = 0;
-	int sequenceNum;
+	int sequenceNum = 0;
+	int lastMsgReceived = 0;
 
 	if (argc != 2) {
 		puts("Uso correto: servidor <porta>");
@@ -87,7 +119,7 @@ int main ( int argc, char *argv[] ){
 	// Obter nome e IP do host
 	gethostname(localhost, MAXHOSTNAME);
 	if ((hp = gethostbyname( localhost)) == NULL){
-		puts ("Nao consegui meu proprio IP");
+		puts ("[Servidor] Nao consegui meu proprio IP");
 		exit (1);
 	}	
 
@@ -98,12 +130,13 @@ int main ( int argc, char *argv[] ){
 
 	// Abrir socket
 	if ((socket_s = socket(hp->h_addrtype,SOCK_DGRAM,0)) < 0){
-           puts ( "Nao consegui abrir o socket" );
+           puts ( "[Servidor] Nao consegui abrir o socket" );
 		exit ( 1 );
 	}	
 
 	// tempo limite de 10 segundos para evitar 
 	// que o servidor fique esperando por muito tempo
+	// no bloqueio para recebimento
 	int timeout = 20; 
 	struct timeval tv;
 	tv.tv_sec = timeout;
@@ -113,20 +146,37 @@ int main ( int argc, char *argv[] ){
 
 	// Vincular socket com endereco ip local e porta
 	if (bind(socket_s, (struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0){
-		puts ( "Nao consegui fazer o bind" );
+		puts ( "[Servidor] Nao consegui fazer o bind" );
 		exit ( 1 );
 	}
-	
-	printf("servidor pronto\n");
+
+	int totalMsgs = 0;
+	printf("[Servidor] Estou pronto para receber mensagens\n");
+
+	int firstMessage = 1;
 	while(1){
 		cli_addr_len = sizeof(cli_addr);
-		if (recvfrom(socket_s, buf, BUFSIZ, 0, (struct sockaddr *) &cli_addr, &cli_addr_len) > 0)
-			printf("Sou o servidor, recebi a mensagem----> %s\n", buf);
-		else printf("Servidor escutando...\n");
-			
-		if(atoi(buf) == 1){
-			receiveMessages(buf, socket_s, cli_addr, cli_addr_len, messagesOutOfOrder, messagesMissing, &indexErrors);
-			calculateMetrics(buf, socket_s, cli_addr, cli_addr_len, messagesOutOfOrder, indexErrors);
+		if (strlen(buf) == 0){  // Recebe primeira mensagem
+			while (firstMessage == 1 && 
+				   recvfrom(socket_s, buf, BUFSIZ, 0, (struct sockaddr *) &cli_addr, &cli_addr_len) > 0){
+				totalMsgs = atoi(buf);
+				printf("[Servidor] Salvando total de mensagens: %d\n", totalMsgs);
+				memset(buf,'\0', BUFSIZ);
+				firstMessage = 0;
+				break;
+			}	
 		}
+		memset(buf,'\0', BUFSIZ);
+		if (recvfrom(socket_s, buf, BUFSIZ, 0, (struct sockaddr *) &cli_addr, &cli_addr_len) > 0 &&
+			firstMessage == 0){
+			printf("[Servidor] Recebi a mensagem----> %d\n", atoi(buf));
+			if(atoi(buf) == 1){
+				receiveMessages(buf, socket_s, cli_addr, cli_addr_len, messagesOutOfOrder, messagesMissing, totalMsgs);
+				// calculateMetrics(buf, socket_s, cli_addr, cli_addr_len, messagesOutOfOrder, indexErrors, lastMsgReceived);
+			}
+		}else {
+			printf("[Servidor] Escutando...");
+		} 
+		memset(buf,'\0', BUFSIZ);
 	}
 }
